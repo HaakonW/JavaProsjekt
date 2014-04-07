cc86c756aec24ffee141d386b8531953903ab8a2
petnwil <PeterWilhelmsen@Peter-Wilhelmsens-MacBook-Pro.local>
2014-04-07 16:23:10 +0200

    Fult fungerende medisinregistrering!

--- NorskReseptRegister/src/norskreseptregister/gui/MedisinGUI.java
+++ NorskReseptRegister/src/norskreseptregister/gui/MedisinGUI.java
@@ -1,3 +1,5 @@
+package norskreseptregister.gui;
+
 /*
 Filen inneholder GUI for å kunne registere en medisin og legge de i en 
 egenopprettet liste.
@@ -10,6 +12,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
+import norskreseptregister.Medisin;
 import norskreseptregister.Medisinliste;
 
 //package norskreseptregister.gui;
@@ -25,6 +28,7 @@ public abstract class MedisinGUI extends JFrame implements ActionListener
     private JTextField regNavn;
     private JButton regMedisinnavn, visListe;
     private JTextArea utskrift,regAnvisning;
+    private String medisinlisten = "lagreMedisin.txt";
     
     
     public MedisinGUI()
@@ -35,6 +39,14 @@ public abstract class MedisinGUI extends JFrame implements ActionListener
         regMedisinnavn = new JButton("Registrer");
         visListe = new JButton("Vis liste");
         utskrift = new JTextArea(20,20);
+        
+        regMedisinnavn.addActionListener(this);
+        visListe.addActionListener(this);
+        
+        liste = new Medisinliste();
+        
+        //For å lese listen ved oppstart
+        lesObjektFraFil();
         
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
@@ -53,22 +65,93 @@ public abstract class MedisinGUI extends JFrame implements ActionListener
         
     }
     
-    
-    /*Main-metode for å kjøre programmet
-    - Vanlig main-metode nå, må byttes til eventQueue.invokeLater
-    - Legges inn i hovedprogrammet senere. */
-    
-    public static void main(String[] args)
+    //Registrere ny medisin
+    public Medisin nyMedisin()
     {
-        MedisinGUI vindu = new MedisinGUI() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
-
+        if(!regNavn.getText().equals(""))
+        {
+            Medisin ny = new Medisin(regNavn.getText());
+            // Kaller på medisinliste.settInn
+            liste.settInn(ny);
+            utskrift.setText("Medisin registrert:\n " + ny.toString());
+            return ny;
+        }
+        return null;  
     }
     
     
+     // Lese objekt fra fil  - HUSK Å IMPORTER java.io.*;
+    public void lesObjektFraFil()
+    {
+        try(ObjectInputStream innfil = new ObjectInputStream(
+                                        new FileInputStream(medisinlisten)))
+        {
+          liste = (Medisinliste)innfil.readObject(); 
+            
+        }
+        catch(ClassNotFoundException cnfe)
+        {
+           JOptionPane.showMessageDialog(this, cnfe.getMessage());
+           /* Må kanskje legge inn muligheten for å opprette ny liste
+           om det ikke finnes en: liste = new Medisinliste();*/
+        }
+        catch(IOException ioe)
+        {
+          JOptionPane.showMessageDialog(this, "Feil ved lesing, "
+                                         + "ny liste blir opprettet");
+          //Kan opprette ny tom liste her også. Som forslaget i kommentar over
+        }
+    }
+
+     //Lagre objektet på fil - try med ressurser
+    
+    public void skrivObjektTilFil()
+    {
+        try(ObjectOutputStream utfil = new ObjectOutputStream(
+                                        new FileOutputStream(medisinlisten)))
+        {
+           utfil.writeObject(liste);
+           utfil.close();
+        }
+        catch(NotSerializableException nse)
+        {
+           JOptionPane.showMessageDialog(this,"Objektet er ikke serialisert!"); 
+        }
+        catch(FileNotFoundException fnfe)
+        {
+            JOptionPane.showMessageDialog(this, "Finner ikke " + medisinlisten);
+        }
+        catch(IOException ioe)
+        {
+            System.out.println("Kan ikke skrive til fil!");
+        }
+    }
+    
+    
+    public void visRegister()
+    {
+        utskrift.setText(liste.toString());
+    }
+    
+    
+    public void tomFelter()
+    {
+        regNavn.setText("");
+    }
+    
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource() == regMedisinnavn)
+        {
+            nyMedisin();
+            tomFelter();
+        }
+        if(e.getSource() == visListe)
+        {
+            visRegister();
+        }
+    }
+    
+   
 }
