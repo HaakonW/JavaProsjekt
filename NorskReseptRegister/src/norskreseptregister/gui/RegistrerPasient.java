7ab8af521a240b90dd13787f89fd3047e55f95ef
HaakonW <haakonwinther@gmail.com>
2014-04-09 15:10:44 +0200

    griddet pasient

--- NorskReseptRegister/src/norskreseptregister/gui/RegistrerPasient.java
+++ NorskReseptRegister/src/norskreseptregister/gui/RegistrerPasient.java
@@ -23,20 +23,24 @@ public class RegistrerPasient extends JPanel implements ActionListener
     private PasientRegister pasientRegister;
     private String pasientliste = "pasientliste.txt";
     private JTextField fornavnfelt, etternavnfelt, fodselsdatofelt;
+    private JLabel fornavnlabel, etternavnlabel, fodsellabel;
     private JButton regPasient, visListe;
     private JTextArea utskrift;
     private String medisinlisten = "lagreMedisin.txt";
+    private JPanel panel1, panel2, panel3, panel4, panel5, panel6;
     
     public RegistrerPasient(PasientRegister pasientRegister)
     {
         this.pasientRegister = pasientRegister;
-        fornavnfelt = new JTextField(40);
-        etternavnfelt = new JTextField(40);
-        fodselsdatofelt = new JTextField(40);
+        fornavnfelt = new JTextField(20);
+        etternavnfelt = new JTextField(20);
+        fodselsdatofelt = new JTextField(20);
+        fornavnlabel = new JLabel("Fornavn");
+        etternavnlabel = new JLabel("Etternavn");
+        fodsellabel = new JLabel("Fødselsdato");
         regPasient = new JButton("Registrer");
         visListe = new JButton("Vis liste");
-        utskrift = new JTextArea(15,45);
-        
+        utskrift = new JTextArea(20,20);        
         regPasient.addActionListener(this);
         visListe.addActionListener(this);
         
@@ -45,15 +49,56 @@ public class RegistrerPasient extends JPanel implements ActionListener
         //For å lese listen ved oppstart
         //lesObjektFraFil();
         
-        add( new JLabel("Fornavn   "));
-        add(fornavnfelt);
-        add( new JLabel("Etternavn   "));
-        add(etternavnfelt);
-        add( new JLabel("Fødselsdato   "));
-        add(fodselsdatofelt);
-        add(utskrift);
-        add(regPasient);
-        add(visListe);
+      
+        ///PANEL START////
+       panel1 = new JPanel();
+       panel1.add(fornavnlabel);
+       panel1.add(fornavnfelt);
+       
+       panel2 = new JPanel();
+       panel2.add(etternavnlabel);
+       panel2.add(etternavnfelt);
+       
+       panel3 = new JPanel();
+       panel3.add(fodsellabel);
+       panel3.add(fodselsdatofelt);
+       
+       panel4 = new JPanel();
+       panel4.add(regPasient);
+       panel4.add(visListe);
+       
+       panel5 = new JPanel();
+       panel5.add(utskrift);
+        
+        
+        /// GRID START ///
+       GridBagConstraints gc = new GridBagConstraints();
+       setLayout(new GridBagLayout());
+       
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.insets = new Insets(10, 5, 5, 10);
+        gc.anchor = GridBagConstraints.WEST;
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        add(panel1, gc);
+        
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(panel2, gc);
+        
+        gc.gridx = 0;
+        gc.gridy = 2;
+        gc.anchor = GridBagConstraints.WEST;
+        add(panel3, gc);
+        
+        gc.gridx = 0;
+        gc.gridy = 3;
+        add(panel4, gc);
+        
+        gc.gridx = 2;
+        gc.gridy = 0;
+        gc.gridheight = 6;
+        add(panel5, gc);
         
         utskrift.setEditable(false); 
     }//end of konstruktør
