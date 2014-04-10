595251bc70681524eada535a6830bf62059db7d3
HaakonW <haakonwinther@gmail.com>
2014-04-10 13:44:05 +0200

    lollet litt iconer

--- NorskReseptRegister/src/norskreseptregister/gui/AdminMainHjemGUI.java
+++ NorskReseptRegister/src/norskreseptregister/gui/AdminMainHjemGUI.java
@@ -12,16 +12,31 @@ import javax.swing.*;
 class AdminMainHjemGUI extends JPanel
 {
     private JButton endreLisens, medisin;
-    private JPanel panel2;
+    private JPanel panel2, panel1;
+    private JLabel test1, test2;
 
     public AdminMainHjemGUI()
     {
         endreLisens = new JButton("Endre Lisens");
         medisin = new JButton("Registrer Medisin");
-
+        ImageIcon modLeg = new ImageIcon("src/norskreseptregister/gui/bilder/Refresh-icon.png");
+        ImageIcon regPill = new ImageIcon("src/norskreseptregister/gui/bilder/epille.png");
+        
+        test1 = new JLabel();
+        test1.setIcon(modLeg);
+        
+        test2 = new JLabel();        
+        test2.setIcon(regPill);
+        
         panel2 = new JPanel();
         panel2.add(endreLisens);
         panel2.add(medisin);
+        
+        panel1 = new JPanel();
+        panel1.add(test1);
+        panel1.add(test2);
+        
+        //panel1.add(regPill):
         
         GridBagConstraints gc = new GridBagConstraints();
         setLayout(new GridBagLayout());
@@ -30,10 +45,12 @@ class AdminMainHjemGUI extends JPanel
         gc.insets = new Insets(10, 5, 5, 10);
         gc.anchor = GridBagConstraints.WEST;
         gc.fill = GridBagConstraints.HORIZONTAL;
-        //add(panel1, gc);
+        add(panel1, gc);
         
         gc.gridx = 0;
         gc.gridy = 1;
         add(panel2, gc);
+        
+        
     }
 }//end of class AdminMainHjemGUI
