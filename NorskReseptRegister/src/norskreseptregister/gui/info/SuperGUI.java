220d026be51b54e2248f2fb5b465eb834ef27803
Henrik Fischer <henrikfb@icloud.com>
2014-04-11 11:56:29 +0200

    endrert litt på superGUI

--- NorskReseptRegister/src/norskreseptregister/gui/info/SuperGUI.java
+++ NorskReseptRegister/src/norskreseptregister/gui/info/SuperGUI.java
@@ -1,15 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Filen inneholder klassen SuperGUI.
+Laget av Henrik Fischer Bjelland 
+Sist endret 11-04-2014 
+*/
 
 package norskreseptregister.gui.info;
 
-/**
- *
- * @author Bandet
- */
-public class SuperGUI {
+import javax.swing.JPanel;
 
+public abstract class SuperGUI extends JPanel 
+{
+    public SuperGUI()
+    {
+        
+    }
 }
