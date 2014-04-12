824473d4a2f5766a68d5b98e92b2d25ad53a8783
HaakonW <haakonwinther@gmail.com>
2014-04-12 16:37:01 +0200

    legeinfogui

--- NorskReseptRegister/src/norskreseptregister/gui/info/LegeInfoGUI.java
+++ NorskReseptRegister/src/norskreseptregister/gui/info/LegeInfoGUI.java
@@ -10,6 +10,34 @@ package norskreseptregister.gui.info;
  *
  * @author Bandet
  */
-public class LegeInfoGUI {
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
 
+public class LegeInfoGUI 
+{
+    private JTextField fornavnfelt, etternavnfelt;
+    private JTextArea utskrift;
+    private JLabel fornavnlabel, etternavnlabel;
+    private JButton knapp1, knapp2, knapp3;
+    private JRadioButton a, b, c;
+    
+    public LegeInfoGUI()
+    {
+        fornavnlabel = new JLabel("Fornavn   ");
+        etternavnlabel = new JLabel("Etternavn ");
+        etternavnfelt = new JTextField(20);
+        fornavnfelt = new JTextField(20);
+        knapp1 = new JButton("Vis alle leger");
+        knapp2 = new JButton("Vis alle resepeter på lege");
+        knapp3 = new JButton("Vis resepter i kategori");
+        a = new JRadioButton("A");
+        b = new JRadioButton("B");
+        c = new JRadioButton("C");
+        
+        
+        
+    }
+    
 }
