4f27eaf6147e16521f1460ca0cde4a02339bd58c
petnwil <PeterWilhelmsen@Peter-Wilhelmsens-MacBook-Pro.local>
2014-04-22 16:56:05 +0200

    Regex tull!

---
+++ NorskReseptRegister/src/norskreseptregister/gui/regGUI/RegEx.java
@@ -0,0 +1,33 @@
+/*
+ Testklasse for regex.
+
+Laget av Peter Wilhelmsen
+
+Sist lagret 22-04-14
+ */
+
+package norskreseptregister.gui.regGUI;
+
+public class RegEx
+{
+    private String fornavn;
+    
+    private void testFornavn()
+    {
+        fornavn = fornavnfelt.getText();
+        fornavn.matches("[a-zA-Z]");
+    }
+    
+}
+
+
+/*
+http://www.regular-expressions.info/java.html
+
+http://stackoverflow.com/questions/4450045/difference-between-matches-and-find-in-java-regex
+
+http://www.tutorialspoint.com/java/java_string_matches.htm
+
+http://java67.blogspot.no/2012/09/java-string-matches-example-regular-expression.html
+
+*/
