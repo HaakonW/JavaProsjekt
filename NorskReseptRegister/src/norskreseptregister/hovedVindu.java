f312058c078266a1c0aa27c872480e6ee553f782
petnwil <PeterWilhelmsen@Peter-Wilhelmsens-MacBook-Pro.local>
2014-04-23 11:37:15 +0200

    Menybar

--- NorskReseptRegister/src/norskreseptregister/hovedVindu.java
+++ NorskReseptRegister/src/norskreseptregister/hovedVindu.java
@@ -34,14 +34,15 @@ public class hovedVindu extends JFrame
     private Menylytter lytter;
 
     private JMenuBar menylinje;
-    private JMenu addmeny;
-    private JMenu addmeny2;
-    private JMenu addmeny3;
-    private JMenu addmeny4;
+    private JMenu valg1;
+    private JMenu valg2;
+    private JMenu valg3;
+    private JMenu valg4;
 
     private JMenuItem submeny1;
     private JMenuItem submeny2;
     private JMenuItem submeny3;
+    private JMenuItem submeny4;
     private RegisterSystem system;
     private final Medisinliste medisinliste;
 
@@ -51,26 +52,33 @@ public class hovedVindu extends JFrame
         lytter = new Menylytter();
 
         menylinje = new JMenuBar();
-        addmeny = new JMenu("Admin");
-        addmeny2 = new JMenu("Registrere");
-        addmeny3 = new JMenu("Informasjon");
-        addmeny4 = new JMenu("Statistikk");
+        valg1 = new JMenu("Admin");
+        valg1.setMnemonic('A');
+        valg2 = new JMenu("Registrere");
+        valg2.setMnemonic('R');
+        valg3 = new JMenu("Informasjon");
+        valg3.setMnemonic('I');
+        valg4 = new JMenu("Statistikk");
+        valg4.setMnemonic('S');
         submeny1 = new JMenuItem("Gå inn");
         submeny2 = new JMenuItem("Åpne vinduet");
         submeny3 = new JMenuItem("Klikk videre");
+        submeny4 = new JMenuItem("Vis statistikk");
+        
+        menylinje.add(valg1);
+        menylinje.add(valg2);
+        menylinje.add(valg3);
+        menylinje.add(valg4);
 
-        menylinje.add(addmeny);
-        menylinje.add(addmeny2);
-        menylinje.add(addmeny3);
-        menylinje.add(addmeny4);
-
-        addmeny.add(submeny1);
-        addmeny2.add(submeny2);
-        addmeny3.add(submeny3);
+        valg1.add(submeny1);
+        valg2.add(submeny2);
+        valg3.add(submeny3);
+        valg4.add(submeny4);
 
         submeny1.addActionListener(lytter);
         submeny2.addActionListener(lytter);
         submeny3.addActionListener(lytter);
+        submeny4.addActionListener(lytter);
 
         //Setter inn menylinjen i vinduet.
         setJMenuBar(menylinje);
@@ -163,6 +171,16 @@ public class hovedVindu extends JFrame
 
                 hovedvindu.add(internvindu3);
                 internvindu3.setVisible(true);
+            }
+            if(e.getSource() == submeny4)
+            {
+                JInternalFrame internvindu4 = new JInternalFrame(
+                "Statistikk",true, true, true,true);
+                
+                internvindu4.setSize(300,300);
+                internvindu4.pack();
+                hovedvindu.add(internvindu4);
+                internvindu4.setVisible(true);
             }
         }//end of method actionPerformed
     } //end of Menylytter
