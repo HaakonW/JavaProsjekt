35270d662b77f0d01d48587383126302ea1d1723
petnwil <peter.n.wilhelmsen@gmail.com>
2014-04-25 17:22:20 +0200

    tabell med fargeklikk og printmulighet

--- NorskReseptRegister/src/norskreseptregister/Statistikk/tabell.java
+++ NorskReseptRegister/src/norskreseptregister/Statistikk/tabell.java
@@ -10,7 +10,15 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.text.MessageFormat;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
@@ -141,17 +149,60 @@ class tabellFrame extends JFrame
         Medisintabell tab = new Medisintabell();
         tabellet = new Radalterneringstabell(tab);
         
+        JMenuBar menylinje = new JMenuBar();
+        JMenu meny1 = new JMenu("Valg");
+        JMenuItem valg1 = new JMenuItem("Print tabell");
+        valg1.addActionListener(new printeLytter());
+        
+        menylinje.add(meny1);
+        meny1.add(valg1);
+        setJMenuBar(menylinje);
+        
         Container c = getContentPane();
         c.add(new JScrollPane(tabellet), BorderLayout.CENTER);
-    }   
+    }
+    
+    //For å printe
+    class printeLytter implements ActionListener
+    {
+        public void actionPerformed( ActionEvent e)
+        {
+            try
+            {
+                /*tabellet.print(JTable.PrintMode.NORMAL,
+                        new MessageFormat("Medisinstatistikk"),
+                        new MessageFormat("side{0,number}"));*/
+                tabellet.print(JTable.PrintMode.NORMAL);
+            }
+            catch(PrinterException pep)
+            {
+                pep.printStackTrace();
+            }
+        }
+    }
 }
 
 // Setter farge på annenhver rad i tabellen
 class Radalterneringstabell extends JTable
 {
+    private boolean printUt = false;
+    
     public Radalterneringstabell(TableModel tab)
     {
         super(tab);
+    }
+    
+    public void print(Graphics g)
+    {
+        printUt = true;
+        try
+        {
+            super.print(g);
+        }
+        finally
+        {
+            printUt = false;
+        }
     }
     
     public Component prepareRenderer(
@@ -159,16 +210,26 @@ class Radalterneringstabell extends JTable
     {
         Component c = super.prepareRenderer(rendrer, rad, kolonne);
         
-        if( rad % 2 == 0 && !isCellSelected(rad,kolonne))
+        if(printUt)
         {
             c.setBackground(getBackground());
         }
+        else if( rad % 2 == 0 && !isCellSelected(rad,kolonne))
+        {
+            c.setBackground(Color.LIGHT_GRAY);
+        }
+        else if( isCellSelected(rad,kolonne)) //gir farge når kolonne klikkes
+        {
+            c.setBackground(Color.ORANGE);
+        }
         else
         {
-            c.setBackground(Color.CYAN);
+            c.setBackground(getBackground());
         }
         return c;
     }
 }
+
+
 
 
