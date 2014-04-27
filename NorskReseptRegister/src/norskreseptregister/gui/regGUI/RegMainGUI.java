78db092b5cde50213cdb1625a9a945d6c4e97a51
HaakonW <haakonwinther@gmail.com>
2014-04-27 14:35:41 +0200

    KOMMENTERT OG RYDDET

--- NorskReseptRegister/src/norskreseptregister/gui/regGUI/RegMainGUI.java
+++ NorskReseptRegister/src/norskreseptregister/gui/regGUI/RegMainGUI.java
@@ -1,12 +1,11 @@
 /*
  Filen inneholder klassen RegMainGUI.
  Laget av  Henrik Fischer Bjelland, Haakon Winther, Peter Wilhelmsen 
- Sist endret 17-04-2014 
+ Sist endret 27-04-2014 
  */
 package norskreseptregister.gui.regGUI;
 
 import java.awt.*;
-import java.awt.event.WindowEvent;
 import javax.swing.*;
 import norskreseptregister.ObjektKlasser.Medisinliste;
 import norskreseptregister.Reg.RegisterSystem;
@@ -23,13 +22,13 @@ public class RegMainGUI extends JPanel
 
     public RegMainGUI(RegisterSystem system, Medisinliste medisinliste)
     {
-        super(new GridLayout(1, 1));
         this.system = system;
         this.medisinliste = medisinliste;
         tab1 = new RegistrerPasient(system.getPasientRegister());
         tab2 = new RegistrerLege(system.getLegeRegister());
         tab3 = new RegistrerResept(system, medisinliste);
         tabbedPane = new JTabbedPane();
+
         ImageIcon hjem = new ImageIcon("src/norskreseptregister/gui/bilder/hjem.png");
         ImageIcon doktor = new ImageIcon("src/norskreseptregister/gui/bilder/doktor.png");
         ImageIcon pasienten = new ImageIcon("src/norskreseptregister/gui/bilder/person.png");
@@ -39,7 +38,6 @@ public class RegMainGUI extends JPanel
         tabbedPane.addTab("Registrer Pasient", pasienten, tab1, "Registrer pasient");
         tabbedPane.addTab("Registrer Lege", doktor, tab2, "Registrer Lege");
         tabbedPane.addTab("Registrer Resept", resept, tab3, "Registrer Resept");
-        //tabbedPane.addTab("Registrer Resept", icon, tab3, "Resept");
         add(tabbedPane);
     }
 
@@ -48,23 +46,22 @@ public class RegMainGUI extends JPanel
         return tabbedPane;
     }
 
-    private static void createAndShowGUI(final RegisterSystem system, Medisinliste medisinliste)
+    /*private static void createAndShowGUI(final RegisterSystem system, Medisinliste medisinliste)
 
-    {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Admin");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(650, 460);
-        frame.getContentPane().add(new RegMainGUI(system, medisinliste), BorderLayout.CENTER);
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
+     {
+     //Create and set up the window.
+     JFrame frame = new JFrame("Admin");
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.setSize(650, 460);
+     frame.getContentPane().add(new RegMainGUI(system, medisinliste), BorderLayout.CENTER);
+     //Display the window.
+     frame.pack();
+     frame.setVisible(true);
+     }
     
-    // GIR FEIL NÅR HOVEDVINDU SKAL KJØRES.Må settes tom 
-    public RegMainGUI()
-    {
+     // GIR FEIL NÅR HOVEDVINDU SKAL KJØRES.Må settes tom 
+     public RegMainGUI()
+     {
 
-    }
-
+     }*/ ///// TEST TEST TEST ///// LAR DETTE STÅ NOEN DAGER OG SER OM ALT FUNGERER SOM DET SKAL /////////
 }//end of class RegMainGUI
--- NorskReseptRegister/src/norskreseptregister/gui/regGUI/RegMainHjemGUI.java
+++ NorskReseptRegister/src/norskreseptregister/gui/regGUI/RegMainHjemGUI.java
@@ -1,22 +1,22 @@
-/*
-Filen inneholder klassen RegMainHjemGUI.
-Laget av  Henrik Fischer Bjelland
-Sist endret 09-04-2014 
-*/
-
+/**
+ * ******************************************************************************************
+ * Filen inneholder klassen RegMainHjemGUI. Det er denne filen som gjør at hjem
+ * siden har de 3 ikonene for endre lisens, registrer medisisn eller statestikk.
+ * Det er her man kan endre bildene, knapper eller utseene for hjem fanen. Laget
+ * av Henrik Fischer Bjelland, Haakon Winther Sist endret 27-04-2014 
+****************************************************************************************************
+ */
 package norskreseptregister.gui.regGUI;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import javax.swing.*;
 
 class RegMainHjemGUI extends JPanel implements ActionListener
 {
+
     private JButton regPasient, regLege, regResept;
-    private JPanel panel1, panel2;
+    private JPanel panel1;
     private JLabel label1, label2, label3;
     private final RegMainGUI regMainGUI;
 
@@ -26,81 +26,88 @@ class RegMainHjemGUI extends JPanel implements ActionListener
         ImageIcon regLeg = new ImageIcon("src/norskreseptregister/gui/bilder/doktor.png");
         ImageIcon regPas = new ImageIcon("src/norskreseptregister/gui/bilder/person.png");
         ImageIcon regRes = new ImageIcon("src/norskreseptregister/gui/bilder/Resept.png");
-        regPasient= new JButton(regPas);
-        regLege = new JButton(regLeg);
-        regResept = new JButton(regRes);
         
+        regPasient = new JButton(regPas);
         regPasient.setFocusPainted(false);
-        regLege.setFocusPainted(false);
-        regResept.setFocusPainted(false);
-        
         regPasient.addActionListener(this);
-        regLege.addActionListener(this);
-        regResept.addActionListener(this);
-        
         regPasient.setToolTipText("Registrer pasient");
-        regLege.setToolTipText("Registrer lege");
-        regResept.setToolTipText("Registrer resept");
         
+        regLege = new JButton(regLeg);
+        regLege.setFocusPainted(false);
+        regLege.addActionListener(this);
+        regLege.setToolTipText("Registrer lege");
+        
+        regResept = new JButton(regRes);
+        regResept.setFocusPainted(false);
+        regResept.addActionListener(this);
+        regResept.setToolTipText("Registrer resept");      
+
         label1 = new JLabel("Registrer Pasient");
-        label1.setFont(new Font ("PT Serif", Font.PLAIN, 20)); 
+        label1.setFont(new Font("PT Serif", Font.PLAIN, 20));
         label2 = new JLabel("Registrer Lege");
-        label2.setFont(new Font ("PT Serif", Font.PLAIN, 20)); 
+        label2.setFont(new Font("PT Serif", Font.PLAIN, 20));
         label3 = new JLabel("Registrer Resept");
-        label3.setFont(new Font ("PT Serif", Font.PLAIN, 20)); 
-               
-        panel2 = new JPanel();
-        panel2.add(regPasient);
-        panel2.add(regLege);
-        panel2.add(regResept);
-         
+        label3.setFont(new Font("PT Serif", Font.PLAIN, 20));
+
+        panel1 = new JPanel();
+        panel1.add(regPasient);
+        panel1.add(regLege);
+        panel1.add(regResept);
+        
+        /***
+         START INNSETTINGEN AV ELEMENTENE I LAYOUTEN
+         OPPRETTER EN INT X OG INT Y FOR Å ENKELT KUNNE UTVIDE
+         MED FLERE ELEMENTER        
+         INSETS ER AVSTAND FRA TOP, VENSTRE, BUNN OG HØYRE
+         ***/
+        
+
         GridBagConstraints gc = new GridBagConstraints();
         setLayout(new GridBagLayout());
-       
+
         int x = 0;
-        int y= 0;
-     
+        int y = 0;
+
         gc.gridx = 0;
         gc.gridy = ++y;
-        gc.insets = new Insets(10,10,10,10);
+        gc.insets = new Insets(10, 10, 10, 10);
         add(regPasient, gc);
-        
+
         gc.gridx = ++x;
         gc.gridy = y;
         add(regLege, gc);
-        
+
         gc.gridx = ++x;
         gc.gridy = y;
         add(regResept, gc);
-        
-        x = 0;
-        
+
+        x = 0; // NULLSTILLER X pga ny rad
+
         gc.gridx = x;
         gc.gridy = ++y;
-        add(label1,gc);
-                
-        gc.gridx = ++x;
-        gc.gridy = y;
-        add(label2, gc); 
-        
-        gc.gridx = ++x;
-        gc.gridy = y;
-        add(label3,gc);
-    }
+        add(label1, gc);
 
-    public void actionPerformed( ActionEvent e )
+        gc.gridx = ++x;
+        gc.gridy = y;
+        add(label2, gc);
+
+        gc.gridx = ++x;
+        gc.gridy = y;
+        add(label3, gc);
+    }
+    /** METODE FOR Å BLI SENDT VIDERE TIL DE ANDRE FANENENE FRA FORSIDEN
+        DETTE ER FOR Å BLI SENDT VIDERE MED KNAPPENE ***/
+    public void actionPerformed(ActionEvent e)
     {
-       if (e.getSource() ==  regPasient)
-       {
-           regMainGUI.getTabbedPane().setSelectedIndex(1);
-       } 
-       else if (e.getSource() == regLege)
-       {
-           regMainGUI.getTabbedPane().setSelectedIndex(2); 
-       }
-        else if (e.getSource() == regResept)
-       {
+        if (e.getSource() == regPasient)
+        {
+            regMainGUI.getTabbedPane().setSelectedIndex(1);
+        } else if (e.getSource() == regLege)
+        {
+            regMainGUI.getTabbedPane().setSelectedIndex(2);
+        } else if (e.getSource() == regResept)
+        {
             regMainGUI.getTabbedPane().setSelectedIndex(3);
-       }
+        }
     }
 }//end of class RegMainHjemGUI
