43be0ae2ba1ed61f03694f7dee1a02fcb4058421
Henrik Fischer <henrikfb@icloud.com>
2014-04-30 11:52:51 +0200

    endret feilmelding til å være inni boksen

--- NorskReseptRegister/src/norskreseptregister/LoggInn.java
+++ NorskReseptRegister/src/norskreseptregister/LoggInn.java
@@ -6,9 +6,10 @@
  */
 package norskreseptregister;
 
-import javax.swing.JFrame;
-import javax.swing.*;
+import java.awt.Color;
 import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.JFrame;
 import norskreseptregister.ObjektKlasser.Medisinliste;
 import norskreseptregister.Reg.RegisterSystem;
 import norskreseptregister.gui.AdminMain;
@@ -17,7 +18,7 @@ public class LoggInn extends JFrame
 {
     private JButton loggInn;
     private JPanel panel1;
-    private JLabel label1, label2, label3;
+    private JLabel label1, label2, label3,label4;
     private JTextField brukerfelt;
     private JPasswordField passordfelt;
     private final RegisterSystem system;
@@ -35,6 +36,8 @@ public class LoggInn extends JFrame
         label1 = new JLabel("Denne siden er kun for helsedirektoratet.");
         label2 = new JLabel("Brukernavn:");
         label3 = new JLabel("Passord:");
+        label4 = new JLabel("Feil brukernavn eller passord. Prøv igjen!");
+        label4.setForeground(Color.red);
         setSize(300, 200);
         setLocation(500, 280);
         panel1.setLayout(null);
@@ -42,9 +45,11 @@ public class LoggInn extends JFrame
         label1.setBounds(20, 10, 300, 30);
         label2.setBounds(10, 50, 100, 20);
         label3.setBounds(10, 85, 100, 20);
+        label4.setBounds(170, 120, 80, 20);
         brukerfelt.setBounds(90, 50, 150, 20);
         passordfelt.setBounds(90, 85, 150, 20);
         loggInn.setBounds(130, 120, 80, 20);
+        label4.setBounds(23, 150, 300, 20);
 
         panel1.add(label1);
         panel1.add(label2);
@@ -52,6 +57,9 @@ public class LoggInn extends JFrame
         panel1.add(brukerfelt);
         panel1.add(passordfelt);
         panel1.add(loggInn);
+        panel1.add(label4);
+        
+        label4.setVisible(false);
 
         getContentPane().add(panel1);
         setVisible(true);
@@ -80,7 +88,7 @@ public class LoggInn extends JFrame
                 } 
                 else
                 {
-                    JOptionPane.showMessageDialog(null, "Wrong Password / Username");
+                    label4.setVisible(true);
                     brukerfelt.setText("");
                     passordfelt.setText("");
                     brukerfelt.requestFocus();
--- NorskReseptRegister/src/norskreseptregister/gui/tabell.java
+++ NorskReseptRegister/src/norskreseptregister/gui/tabell.java
@@ -31,118 +31,16 @@ public class tabell
     
 }
 
-class Medisintabell extends AbstractTableModel
-{
-    private String[] kolonnenavn =
-    {
-        "Medisin", "Januar","Februar","Mars","April","Mai","Juni","Juli",
-        "August","September","Oktober","November","Desember"
-    };
-    
-    private Object[][] celler = 
-    {
-        {
-            "Medisin 1", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        },
-        {
-            "Medisin 2", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        },
-        {
-            "Medisin 3", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        },
-        {
-            "Medisin 4", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)  
-        },
-        {
-            "Medisin 5", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        },
-        {
-            "Medisin 6", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)  
-        },
-        {
-            "Medisin 7", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        },
-        {
-            "Medisin 8", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        },
-        {
-            "Medisin 9", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        },
-        {
-            "Medisin 10", new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10),new Integer(10),new Integer(10),new Integer(10),
-            new Integer(10)
-        }
-    };
-    
-    //For å få satt eget kolonnenavn
-    public String getColumnName(int kolonne)
-    {
-        return kolonnenavn[ kolonne ];
-    }
-    
-    //For å informere tabellmodellen om kolonnenes datatyper
-    public Class getColumnClass(int kolonne)
-    {
-        
-        return celler[0][kolonne].getClass();
-    }
-    
-    public int getColumnCount()
-    {
-        return celler[0].length;
-    }
-    
-    public int getRowCount()
-    {
-        return celler.length;
-    }
-    
-    public Object getValueAt(int rad, int kolonne)
-    {
-        return celler[rad][kolonne];
-    }
-}
-
 // Legger tabellen inn i en frame
 class tabellFrame extends JPanel
 {
     public Radalterneringstabell tabellet;
+    private final MedisinTabell medisinTabell;
     
-    public tabellFrame()
+    public tabellFrame(MedisinTabell medisinTabell)
     {
-        Medisintabell tab = new Medisintabell();
-        tabellet = new Radalterneringstabell(tab);
-        
-        
+        this.medisinTabell = medisinTabell;
+        tabellet = new Radalterneringstabell(medisinTabell);
         add(new JScrollPane(tabellet), BorderLayout.CENTER);
         
     }
